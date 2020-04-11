55936132d6560da714e9cd07cceac96095b7d183
Łukasz Langa <lukasz@langa.pl>
2020-04-11 21:19:02 +0200

    Add basic humanized forms of datetimes for English

--- commonplace/convenience.py
+++ commonplace/convenience.py
@@ -125,3 +125,40 @@ def sha1(arg: Union[str, bytes]) -> bytes:
         arg = arg.encode("utf8")
     hash.update(arg)
     return hash.digest()
+
+
+def get_english_timedelta_description(delta: datetime.timedelta) -> str:
+    days = delta.days + delta.seconds / 3600 / 24
+    hours = delta.seconds / 3600
+    minutes = delta.seconds % 3600 / 60
+    seconds = delta.seconds % 3600 % 60
+
+    if days > 0.5:
+        if 0.9 < days < 1:
+            days = 0.9
+        unit = "day" if days == 1 else "days"
+        return (
+            f"{days:.0f} {unit}" if days == int(days) else f"{days:.1f} {unit}"
+        )
+    elif hours > 0.5:
+        if 0.9 < hours < 1:
+            hours = 0.9
+        unit = "hour" if hours == 1 else "hours"
+        if hours == int(hours):
+            return f"{hours:.0f} {unit}"
+        return f"{hours:.1f} {unit}"
+    elif minutes > 0.5:
+        if 0.9 < minutes < 1:
+            minutes = 0.9
+        unit = "minute" if minutes == 1 else "minutes"
+        if minutes == int(minutes):
+            return f"{minutes:.0f} {unit}"
+        return f"{minutes:.1f} {unit}"
+    else:
+        unit = "second" if seconds == 1 else "seconds"
+        return f"{seconds} {unit}"
+
+
+def get_english_dt_description_from_now(ts: datetime.datetime) -> str:
+    delta = nowtz() - ts
+    return get_english_timedelta_description(delta)
