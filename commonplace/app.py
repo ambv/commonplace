09cf885563b643aba38f7bb38cfb41cf837ec3f6
Łukasz Langa <lukasz@langa.pl>
2021-03-18 11:55:00 +0100

    Add templating

--- commonplace/app.py
+++ commonplace/app.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import Response
+from starlette.templating import Jinja2Templates
 
 app = Starlette(debug=True)
+current_dir = Path(__file__).parent
+templates = Jinja2Templates(directory=str(current_dir / "templates"))
 
 
 @app.route("/error")
@@ -14,4 +19,8 @@ async def error(request: Request) -> Response:
 
 @app.exception_handler(404)
 async def not_found(request: Request, exc: Exception) -> Response:
-    return Response(b"not found")
+    return templates.TemplateResponse(
+        name="404.html",
+        context={"request": request, "domain": "lukasz.langa.pl", "title": "404!"},
+        status_code=404,
+    )
