a8792ae5fb0e798c378650a76d9d8c842a64adf2
Łukasz Langa <lukasz@langa.pl>
2021-03-18 12:42:31 +0100

    Add 500 handler

--- commonplace/app.py
+++ commonplace/app.py
@@ -31,3 +31,12 @@ async def not_found(request: Request, exc: Exception) -> Response:
         context={"request": request, "domain": "lukasz.langa.pl", "title": "404!"},
         status_code=404,
     )
+
+
+@app.exception_handler(500)
+async def internal_server_error(request: Request, exc: Exception) -> Response:
+    return templates.TemplateResponse(
+        name="500.html",
+        context={"request": request, "domain": "lukasz.langa.pl", "title": "500!"},
+        status_code=500,
+    )
